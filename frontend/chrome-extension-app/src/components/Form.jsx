514b1a344e0fed946100171b151cab26dbe05c3c
Justin Ankrom <justinankrom1@gmail.com>
2024-03-08 09:14:43 +0100

    Add rough draft of footer form

--- frontend/chrome-extension-app/src/components/Form.jsx
+++ frontend/chrome-extension-app/src/components/Form.jsx
@@ -1,6 +1,66 @@
+import TextField from "@mui/material/TextField"
+import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+import SendIcon from "@mui/icons-material/Send"
+import { green } from "@mui/material/colors"
+
 const Form = () => {
   return (
-    <div>Form</div>
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "75%" },
+        marginTop: "10px",
+      }}
+      noValidate
+      autoComplete="off"
+      method="POST"
+      textAlign="center"
+    >
+      <TextField
+        id="outlined-number"
+        label="Company URL"
+        type="text"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <TextField
+        id="outlined-number"
+        label="Company Name"
+        type="text"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <TextField
+        id="outlined-number"
+        label="Position"
+        type="text"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <TextField
+        id="outlined-number"
+        label="Position Number"
+        type="text"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <TextField
+        id="outlined-number"
+        label="Date"
+        type="date"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <Button id="submit-btn" variant="contained" endIcon={<SendIcon />}>
+        Send
+      </Button>
+    </Box>
   )
 }
 export default Form
