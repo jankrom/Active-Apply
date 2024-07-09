65c7487efa86b3baa034c68efc42623a23a1c517
Justin Ankrom <justinankrom1@gmail.com>
2024-07-08 20:17:29 -0400

    Remove unnecessarry files

--- active-apply/app/layout.tsx
+++ active-apply/app/layout.tsx
@@ -1,20 +1,15 @@
-import { GeistSans } from "geist/font/sans";
-import "./globals.css";
-
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+import { GeistSans } from "geist/font/sans"
+import "./globals.css"
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
-};
+  title: "Active Apply",
+  description: "Easily track where you apply",
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
@@ -24,5 +19,5 @@ export default function RootLayout({
         </main>
       </body>
     </html>
-  );
+  )
 }
