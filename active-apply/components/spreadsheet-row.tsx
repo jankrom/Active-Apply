da9e88347a4b8e2d70641ddc9b95bec605abbd27
Justin Ankrom <justinankrom1@gmail.com>
2024-07-12 01:17:27 -0400

    Add colors to status on spreadsheet row

--- active-apply/components/spreadsheet-row.tsx
+++ active-apply/components/spreadsheet-row.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/utils"
+
 interface Props {
   spreadsheetRow: {
     id: number
@@ -15,25 +17,39 @@ interface Props {
 const SpreadsheetRow = ({ spreadsheetRow, spreadsheetId }: Props) => {
   return (
     <>
-      <div className="w-full grid grid-cols-6 bg-gray-50 py-4 px-2 items-center border border-1">
-        <div className="text-center overflow-auto">
+      <div className="w-full grid grid-cols-6 bg-gray-50 items-center border border-1">
+        <div className="text-center overflow-auto py-4 px-2">
           {spreadsheetRow.companyName}
         </div>
-        <div className="text-center overflow-auto">{spreadsheetRow.jobUrl}</div>
-        <div className="text-center overflow-auto">
+        <div className="text-center overflow-auto py-4 px-2">
+          {spreadsheetRow.jobUrl}
+        </div>
+        <div className="text-center overflow-auto py-4 px-2">
           {spreadsheetRow.position}
         </div>
-        <div className="text-center overflow-auto">
+        <div className="text-center overflow-auto py-4 px-2">
           {spreadsheetRow.positionNumber}
         </div>
-        <div className="text-center overflow-auto">
+        <div className="text-center overflow-auto py-4 px-2">
           {new Intl.DateTimeFormat("en-US", {
             year: "numeric",
             month: "2-digit",
             day: "2-digit",
           }).format(spreadsheetRow.date)}
         </div>
-        <div className="text-center overflow-auto">{spreadsheetRow.status}</div>
+        <div
+          className={`text-center overflow-auto h-full flex items-center justify-center ${
+            spreadsheetRow.status === "Interviewing"
+              ? "bg-yellow-300"
+              : spreadsheetRow.status === "Accepted"
+              ? "bg-green-300"
+              : spreadsheetRow.status === "Rejected"
+              ? "bg-red-300"
+              : "bg-gray-50"
+          }`}
+        >
+          {spreadsheetRow.status}
+        </div>
       </div>
     </>
   )
